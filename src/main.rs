102805efae3e133a8e5d88a3b57c4e67be5b0191
danyrey <danyrey@gmx.net>
2023-04-25 22:21:48 +0200

    basic 3D scene from examples

--- src/main.rs
+++ src/main.rs
@@ -37,10 +37,47 @@ fn keyboard_input(keys: Res<Input<KeyCode>>) {
     }
 }
 
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(5.0).into()),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    // cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.9, 0.3, 0.6))) // not working yet
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
         .add_system(bevy::window::close_on_esc)
         .add_system(keyboard_input)
         .run();
